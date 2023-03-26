c88300ba7e196303e902929413f5bddb75bbc872
Olaf Mihari <olafnjenga@gmail.com>
2023-03-26 23:19:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books").then((raw) => raw.json()).then((data) => renderBooks(data));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  for (const book of books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
